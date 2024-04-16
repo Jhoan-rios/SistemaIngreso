9dd4fe27e33b1c1ead1d1705e96c84463097affc
Jhoan Rios <jhoanriosmorales@mgmail.com>
2024-04-16 11:59:10 -0500

    Creacion de modelos

---
+++ Models/Historia.cs
@@ -0,0 +1,9 @@
+namespace SistemaIngreso.Models
+{
+    public class Historia
+    {
+        public int Id { set; get; }
+        public DateOnly HoraEntrada { get; set; }
+        public DateOnly HoraSalida { get; set; }
+    }
+}
